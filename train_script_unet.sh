2b23f5d5748d48481dd47faa679535ab1822f258
Nikita Genze <ga78faf@mytum.de>
2022-06-28 17:59:19 +0000

    updated gitignore

--- train_script_unet.sh
+++ train_script_unet.sh
@@ -3,9 +3,11 @@ cd /home/ngenze/UAVWeedSegmentation
 # Run all experiments in one go
 # TODO: add more experiments
 
-# no dilation, 32x upsampling using resnet50
-
-python3 train.py "unetsmp" "resnet18" --pretrained --run_prefix "check"
-
-# dilation x4, --> 8x upsampling using resnet50
-python3 train.py "unetown" "resnet18" --pretrained --run_prefix "check"
+echo "----- STARTING 1/4 -----"
+python3 train.py "unet" "resnet101" --pretrained --b_bilinear --run_prefix "save"
+echo "----- STARTING 2/4 -----"
+python3 train.py "unet" "resnet50" --pretrained --b_bilinear --run_prefix "save"
+echo "----- STARTING 3/4 -----"
+python3 train.py "unet" "resnet34" --pretrained --b_bilinear --run_prefix "save"
+echo "----- STARTING 4/4 -----"
+python3 train.py "unet" "resnet18" --pretrained --b_bilinear --run_prefix "save"
